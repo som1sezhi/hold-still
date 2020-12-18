1a5e975f87c289d4681a407f0e66021ef40dd15c
som1sezhi <s89jssy22@gmail.com>
2020-12-18 04:32:24 -0500

    continued testing of accelerometer

--- hold-still.js
+++ hold-still.js
@@ -1,7 +1,20 @@
-let text = document.getElementById('accel-x');
+function setupAccel() {
+	let accel = new Accelerometer({frequency: 60});
+	console.log('hi');
+	function updateDisplay(e) {
+		document.getElementById('accel-x').innerHTML = accel.x.toString();
+		document.getElementById('accel-y').innerHTML = accel.y.toString();
+		document.getElementById('accel-z').innerHTML = accel.z.toString();
+	}
+
+	accel.addEventListener('reading', updateDisplay);
+}
+
+let text = document.getElementById('status');
 navigator.permissions.query({name: 'accelerometer'}).then(function(result) {
 	if (result.state === 'granted') {
 		text.innerHTML = 'nice';
+		setupAccel();
 	} else if (result.state === 'prompt') {
 		text.innerHTML = 'prompt';
 	} else if (result.state === 'denied') {
